5bf7398a267b69784f30d2466f4dab062d24722d
tabshi <tabshi@outlook.com>
2016-10-26 08:19:56 +0800

    1026

--- crimemap.py
+++ crimemap.py
@@ -3,18 +3,21 @@ from dbhelper import DBHelper
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
 
 app = Flask(__name__)
 DB = DBHelper()
 
 @app.route("/")
 def home():
-    try:
-        data = DB.get_all_inputs()
-    except Exception as e:
-        print e
-        data = None
-    return render_template("home.html",data=data)
+    crimes = DB.get_all_crimes()
+    crimes = json.dumps(crimes)
+    # try:
+    #     data = DB.get_all_inputs()
+    # except Exception as e:
+    #     print e
+    #     data = None
+    return render_template("home.html",crimes=crimes)
 
 @app.route("/add",methods=["POST"])
 def add():
@@ -23,6 +26,16 @@ def add():
         DB.add_input(data)
     except Exception as e:
         print e
+    return home()
+
+@app.route("/submitcrime", methods=['POST'])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description").encode("utf-8").decode("latin1")
+    DB.add_crime(category,date,latitude,longitude,description)
     return home()
 
 @app.route("/clear")
